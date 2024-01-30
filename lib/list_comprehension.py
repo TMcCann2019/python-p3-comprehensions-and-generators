be003a408dc26d42671dbc239fde1299282d6f22
Tim McCann <timothysmccann@gmail.com>
2024-01-30 07:52:57 -0500

    completed lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
+    even_numbers = [x for x in num_list if x % 2 == 0]
+    return even_numbers
     pass
 
 def make_exclamation(sentence_list):
+    exclamated_sentences = [sentence + '!' for sentence in sentence_list]
+    return exclamated_sentences
     pass
